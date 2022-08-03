3c5fc0c0f45c34acac8aa11ca7cf8758cd748ab8
Valerio <valerio.porpo@gmail.com>
2022-08-03 09:55:56 +0200

    Base animation

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 let innerCircle = document.querySelector('body div.parent-circle div.child-circle')
-innerCircle.style.width = '1%'
-innerCircle.style.height = '1%'
+let animate
+let counter = 0
+let step = 0.001
+
+animate = setInterval(scale, 7)
+
+function getScale(el) {
+    let style = window.getComputedStyle(el)
+    let matrix = new WebKitCSSMatrix(style.transform)
+    let scaleObj = {
+        'x' : matrix.a,
+        'y' : matrix.d,
+    }
+    return scaleObj
+}
+
+function scale() {
+    let xScale = getScale(innerCircle).x
+    let yScale = getScale(innerCircle).y
+    let xScaleNew = xScale + step
+    let yScaleNew = yScale + step
+    innerCircle.style.transform = 'translate(-50%,-50%) scale('+xScaleNew+', '+yScaleNew+')'
+    if(xScaleNew >= 1 || yScaleNew >= 1)
+      clearInterval(animate)
+    console.log()  
+}
