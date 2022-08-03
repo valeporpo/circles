9aac740cb4e4540ea51eb622ddd18ff8c15289d1
Valerio <valerio.porpo@gmail.com>
2022-08-03 12:46:15 +0200

    Images

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let htmlContainer = document.querySelector('body div.html')
 let innerCircle = document.querySelector('body div.html div.circle')
+let imgsName = [
+   '7-samurai',
+   '13-assassins',
+   'iojimbo',
+   'sanjuro',
+   'equinox-flower',
+   'late-spring',
+   'hidden-fortress',
+   'end-of-summer',
+   'valley-of-the-fangs',
+   'the-assassin'
+]
 
 let step = 0.001
 addEventListener("load", function() {
@@ -22,6 +34,8 @@ function createNewCircle(prevEl) {
    el.classList.add('circle')
    let idProg = Number(PrevElId.substring(7)) + 1
    el.id = 'circle-' + idProg
+   let randNum = Math.floor(Math.random() * imgsName.length);
+   el.style.backgroundImage = 'url(img/' + imgsName[randNum] + '.jpg)'
    return el
 }
 
@@ -40,10 +54,11 @@ function startAnimation(el) {
         let yScaleNew = yScale + step
         el.style.transform = 'translate(-50%, -50%) scale('+xScaleNew+', '+yScaleNew+')'
         if(xScaleNew == 0.75 || yScaleNew == 0.75) {
+            
+        } else if(xScaleNew >= 1 || yScaleNew >= 1) {
             let newCircle = createNewCircle(el)
             htmlContainer.append(newCircle)
             startAnimation(newCircle)
-        } else if(xScaleNew >= 1 || yScaleNew >= 1) {
             removeGrandParent(el)
             clearInterval(animation)
         }
