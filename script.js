dc9e7f074a89d97bac2f99e3260e5c920bca22c7
Valerio <valerio.porpo@gmail.com>
2022-08-03 11:16:07 +0200

    Element pop

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let htmlContainer = document.querySelector('body div.html')
 let innerCircle = document.querySelector('body div.html div.circle')
-let animate
-//let counter = 0
+
 let step = 0.001
 addEventListener("load", function() {
     startAnimation(innerCircle)
@@ -17,28 +16,36 @@ function getScale(el) {
     return scaleObj
 }
 
-
-
-function createNewCircle() {
+function createNewCircle(prevEl) {
+   let PrevElId = prevEl.id
    let el = document.createElement('div')
    el.classList.add('circle')
+   let idProg = Number(PrevElId.substring(7)) + 1
+   el.id = 'circle-' + idProg
    return el
 }
 
+function removeGrandParent(currentEl) {
+   if(currentEl.id.substring(7) > 1) {
+    document.querySelector('div.circle#circle-' + Number(currentEl.id.substring(7) - 2)).remove()
+   }
+}
+
 function startAnimation(el) {
-    let animate
-    animate = setInterval(function() {
+    let animation
+    animation = setInterval(function() {
         let xScale = getScale(el).x
         let yScale = getScale(el).y
         let xScaleNew = xScale + step
         let yScaleNew = yScale + step
         el.style.transform = 'translate(-50%, -50%) scale('+xScaleNew+', '+yScaleNew+')'
         if(xScaleNew == 0.75 || yScaleNew == 0.75) {
-            let newCircle = createNewCircle()
+            let newCircle = createNewCircle(el)
             htmlContainer.append(newCircle)
             startAnimation(newCircle)
         } else if(xScaleNew >= 1 || yScaleNew >= 1) {
-            clearInterval(animate)
+            removeGrandParent(el)
+            clearInterval(animation)
         }
     }, 7)
 }
