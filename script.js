396dd6bbc03290d385512adcada0d980b153bf2f
Valerio <valerio.porpo@gmail.com>
2022-08-03 10:48:11 +0200

    Created loop

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let innerCircle = document.querySelector('body div.parent-circle div.child-circle')
+let htmlContainer = document.querySelector('body div.html')
+let innerCircle = document.querySelector('body div.html div.circle')
 let animate
-let counter = 0
+//let counter = 0
 let step = 0.001
-
-animate = setInterval(scale, 7)
+addEventListener("load", function() {
+    startAnimation(innerCircle)
+});
 
 function getScale(el) {
     let style = window.getComputedStyle(el)
@@ -15,13 +17,28 @@ function getScale(el) {
     return scaleObj
 }
 
-function scale() {
-    let xScale = getScale(innerCircle).x
-    let yScale = getScale(innerCircle).y
-    let xScaleNew = xScale + step
-    let yScaleNew = yScale + step
-    innerCircle.style.transform = 'translate(-50%,-50%) scale('+xScaleNew+', '+yScaleNew+')'
-    if(xScaleNew >= 1 || yScaleNew >= 1)
-      clearInterval(animate)
-    console.log()  
+
+
+function createNewCircle() {
+   let el = document.createElement('div')
+   el.classList.add('circle')
+   return el
+}
+
+function startAnimation(el) {
+    let animate
+    animate = setInterval(function() {
+        let xScale = getScale(el).x
+        let yScale = getScale(el).y
+        let xScaleNew = xScale + step
+        let yScaleNew = yScale + step
+        el.style.transform = 'translate(-50%, -50%) scale('+xScaleNew+', '+yScaleNew+')'
+        if(xScaleNew == 0.75 || yScaleNew == 0.75) {
+            let newCircle = createNewCircle()
+            htmlContainer.append(newCircle)
+            startAnimation(newCircle)
+        } else if(xScaleNew >= 1 || yScaleNew >= 1) {
+            clearInterval(animate)
+        }
+    }, 7)
 }
